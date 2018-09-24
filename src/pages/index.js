0885c0f2f521f39b5575d01f515876ab5112a5b3
Adam Watters <adamwatters11@gmail.com>
2018-09-24 18:04:54 -0400

    added pizza

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
 import nametag from '../images/nametag.png'
+import pizza from '../images/pizza.png'
 import girlanddog from '../images/girlanddog.png'
 
 const IndexPage = () => (
   <Layout>
     <img src={nametag} alt="adam watters" />
+    <img src={pizza} alt="eating pizza" />
     <img src={girlanddog} alt="girl and dog" />
   </Layout>
 )
