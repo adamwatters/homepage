1a617bcd83e9033fd43271387ef157d3c03208bc
Adam Watters <adamwatters11@gmail.com>
2018-09-24 19:14:28 -0400

    links page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,24 @@ import Layout from '../components/layout'
 import nametag from '../images/nametag.png'
 import pizza from '../images/pizza.png'
 import girlanddog from '../images/girlanddog.png'
+import links from '../images/links.png'
+import { Link } from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
+    <Link to="/links">
+      {' '}
+      <img
+        style={{
+          position: 'absolute',
+          height: '200px',
+          top: '0',
+          right: '30px',
+        }}
+        src={links}
+        alt="links"
+      />
+    </Link>
     <img src={nametag} alt="adam watters" />
     <img src={pizza} alt="eating pizza" />
     <img src={girlanddog} alt="girl and dog" />
---
+++ src/pages/links.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const SecondPage = () => (
+  <Layout>
+    <Link style={{ paddingTop: '20px' }} to="/">
+      {`<< back`}
+    </Link>
+    <h2 style={{ paddingTop: '20px' }}>elsewhere</h2>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://www.instagram.com/adam.watters/?hl=en"
+    >
+      Instagram
+    </a>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://github.com/adamwatters"
+    >
+      Github
+    </a>
+  </Layout>
+)
+
+export default SecondPage
