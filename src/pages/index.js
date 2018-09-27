5ed48e5c2a4106d99a4a15077595f066a1d735b7
Adam Watters <adamwatters11@gmail.com>
2018-09-27 18:02:42 -0400

    added pictures

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import nametag from '../images/nametag.png'
 import pizza from '../images/pizza.png'
 import drew from '../images/drew.png'
 import girlanddog from '../images/girlanddog.png'
+import joe from '../images/joepearl.png'
+import me from '../images/me.png'
+import kiss from '../images/kiss.png'
 import links from '../images/links.png'
 import { Link } from 'gatsby'
 import './index.css'
@@ -15,9 +18,12 @@ const IndexPage = () => (
       <img className="links-image" src={links} alt="links" />
     </Link>
     <img className="nametag-image" src={nametag} alt="adam watters" />
-    <img src={pizza} alt="eating pizza" />
+    <img src={me} alt="me" />
+    <img src={kiss} alt="kiss" />
     <img src={drew} alt="man with mushroom pizza" />
+    <img src={joe} alt="joe presenting granola bar" />
     <img src={girlanddog} alt="girl and dog" />
+    <img src={pizza} alt="eating pizza" />
   </Layout>
 )
 
