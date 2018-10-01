5af0f4db69013061c62aa4f6d8ac12facaec7469
Adam Watters <adamwatters11@gmail.com>
2018-10-01 08:25:51 -0400

    added boots

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import me from '../images/me.png'
 import kiss from '../images/kissing.png'
 import bobby from '../images/bobby.png'
 import hamburger from '../images/hamburger.png'
+import boots from '../images/boots.png'
 import { Link } from 'gatsby'
 import './index.css'
 
@@ -21,6 +22,7 @@ const IndexPage = () => (
     <img className="nametag-image" src={nametag} alt="adam watters" />
     <img src={me} alt="me" />
     <img src={kiss} alt="kiss" />
+    <img src={boots} alt="boots" />
     <img src={bobby} alt="bobby" />
     <img src={drew} alt="man with mushroom pizza" />
     <img src={joe} alt="joe presenting granola bar" />
