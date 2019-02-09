e4a86017a46011c3c5122e2d5f910ba52909f910
Adam Watters <adamwatters11@gmail.com>
2019-02-08 21:02:02 -0500

    changed to init in js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,1 +1,22 @@
-import CMS from "netlify-cms"
+import CMS, {init} from "netlify-cms"
+
+init({
+  config: {
+    backend: {
+      name: "git-gateway",
+      branch: "master"
+    },
+    media_folder: "static/img",
+    public_folder: "/img",
+    htmlTitle: "Homepage CMS",
+    collections: [{
+      name: "drawings",
+      folder: "src/drawings",
+      create: true,
+      fields: [
+        { label: "Title", name: "title", widget: "string" },
+        { label: "Publish Date", name: "date", widget: "datetime" }
+      ]
+    }]
+  },
+})
